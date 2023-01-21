ef201e6f3dadf349c122e15f6144b042c476986e
Alhaj Musa <alhaj05322@gmail.com>
2023-01-21 15:33:05 -0800

    update agin webserver

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -215,7 +215,7 @@ class WebServer {
           builder.append("\n");
           builder.append("Result is: " + result);
           }catch(Exception e){
-              builder.append("HTTP/1.1 400 Bad Request");
+              builder.append("HTTP/1.1 400 Bad Request\n");
               builder.append("Content-Type: text/html; charset=utf-8\n");
               builder.append("\n");
               //builder.append("Result is: " + result);
